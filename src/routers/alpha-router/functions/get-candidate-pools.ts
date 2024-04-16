5abc854a72bb44d7b2aec69c045d0dd83aa180cf
Jameshsiao <heroms90@gmail.com>
2024-04-16 15:24:56 +0800

    refactor(get-candidate-pools): to resolve the published error
    Cannot read properties of undefined (reading 'DAI_OPTIMISM_SEPOLIA')

--- src/routers/alpha-router/functions/get-candidate-pools.ts
+++ src/routers/alpha-router/functions/get-candidate-pools.ts
@@ -4,14 +4,9 @@ import { FeeAmount } from '@uniswap/v3-sdk';
 import _ from 'lodash';
 
 import {
-  DAI_OPTIMISM_SEPOLIA,
   ITokenListProvider,
   IV2SubgraphProvider,
-  USDC_ARBITRUM_SEPOLIA,
-  USDC_OPTIMISM_SEPOLIA,
-  USDT_OPTIMISM_SEPOLIA,
   V2SubgraphPool,
-  WBTC_OPTIMISM_SEPOLIA
 } from '../../../providers';
 import {
   CELO,
@@ -59,6 +54,11 @@ import {
   WMATIC_POLYGON,
   WMATIC_POLYGON_MUMBAI,
   WXDAI_GNOSIS,
+  DAI_OPTIMISM_SEPOLIA,
+  USDC_ARBITRUM_SEPOLIA,
+  USDC_OPTIMISM_SEPOLIA,
+  USDT_OPTIMISM_SEPOLIA,
+  WBTC_OPTIMISM_SEPOLIA
 } from '../../../providers/token-provider';
 import {
   IV2PoolProvider,
